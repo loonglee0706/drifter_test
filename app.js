eb5d5c59b12f43cc784e36b5ae4561d19265fa95
lifei <loonglee0706@126.com>
2014-07-07 14:40:47 +0800

    study

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var redis = require('./models/redis.js');
+
+var app = express();
+//app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+
+//drop a drift bottle
+/*
+{
+	time : timestamp	//timestamp drifts out of,default Date.now()
+	owner : username || id	drift bottle master
+	type : male || female 	drift bottle type
+	content : drift bottle content
+}
+*/
+//POST owner=xxx&type=xxx&content=xxx[&time=xxx]
+app.post('/', function(req, res){
+	if (!req.body.owner && req.body.type && req.body.content) {
+		return res.json({code: 0, msg: ""});
+	}
+	redis.throw(req.body, function (result) {
+		res.json(result);
+	});
+})
+
+//get a drift bottle
+/*
+@params
+user : username || userid		//unique
+type : all || male || female	//default:all
+*/
+//return value:JSON
+/*
+{
+	code : 0 || 1		identification code 1 success 0 failure
+	msg : string		return message
+	time : timestamp	drop time
+	owner : username || userid	// unique
+	type : male || female
+	content : content
+}
+*/
+//GET /?user=XXX[&typpe=xxx]
+app.get('/', function(req, res){
+	if (!req.query.user) {
+		return res.json({ code : 0, msg : 'Incomplete information' });
+	}
+	redis.pick(req.query, function(result){
+		res.json(result);
+	});
+})
+
+app.listen(3000);
+console.log('listen : 3000');
